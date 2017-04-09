41cedc9c9ef44f32f74a4f288fe3e2b8506811a6
cyliustack <cyliustack@gmail.com>
2017-04-10 06:47:04 +0800

    Add a spark-wc-on-HDFS example

---
+++ spark/wc_hdfs.py
@@ -0,0 +1,12 @@
+from pyspark import SparkConf, SparkContext
+conf = (SparkConf()
+         .setMaster("local")
+         .setAppName("My app")
+         .set("spark.executor.memory", "1g"))
+sc = SparkContext(conf = conf)
+
+text_file = sc.textFile("hdfs://localhost:9000/user/file0.csv")
+counts = text_file.flatMap(lambda line: line.split(" ")) \
+             .map(lambda word: (word, 1)) \
+             .reduceByKey(lambda a, b: a + b)
+counts.saveAsTextFile("hdfs://localhost:9000/user/result_counts")
