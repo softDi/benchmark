314f192514f63aab5f1c316948faca5a5ca671e4
geniuslove5 <geniuslove55@gmail.com>
2017-04-15 22:23:59 +0800

    Modify spark wc

--- spark/wc_hdfs.py
+++ spark/wc_hdfs.py
@@ -5,8 +5,8 @@ conf = (SparkConf()
          .set("spark.executor.memory", "1g"))
 sc = SparkContext(conf = conf)
 
-text_file = sc.textFile("hdfs://localhost:9000/user/file0.csv")
+text_file = sc.textFile("hdfs://hpc0:9000/aihpc/benchmark/wc/README.txt")
 counts = text_file.flatMap(lambda line: line.split(" ")) \
              .map(lambda word: (word, 1)) \
              .reduceByKey(lambda a, b: a + b)
-counts.saveAsTextFile("hdfs://localhost:9000/user/result_counts")
+counts.saveAsTextFile("hdfs://hpc0:9000/aihpc/benchmark/wc/result_counts")
