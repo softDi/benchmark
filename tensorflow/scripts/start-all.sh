41a299e959f7a8562aa4b774ef81ea27749734b6
cyliustack <cyliustack@gmail.com>
2017-04-28 15:24:25 +0800

    Fix bug of tensorflow script

--- tensorflow/scripts/start-all.sh
+++ tensorflow/scripts/start-all.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 if [ "$(hostname)" == "hpc0" ] ; then 
-    echo "As ps0." && exit 0
+ echo "This is Node-$( hostname | sed 's/hpc//g') "
+ echo "Act as a parameter server." && exit 0
 python trainer.py \
     --ps_hosts=hpc0:2222 \
     --worker_hosts=hpc1:2222,hpc2:2222,hpc3:2222 \
     --job_name=ps --task_index=0
 else 
-    echo "This is Node-$(sed \'s/hpc//g\') "
+    echo "This is Node-$(hostname | sed 's/hpc//g') "
     echo "Act as a worker." && exit 0
     python trainer.py \
     --ps_hosts=hpc0:2222 \
