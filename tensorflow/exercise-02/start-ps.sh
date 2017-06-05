f78509fd82bfc368c122a7570a72e0051bfe2b3c
cyliustack <cyliustack@gmail.com>
2017-06-06 03:49:45 +0800

    update tensorflow inception distributed learning

--- tensorflow/exercise-02/start-ps.sh
+++ tensorflow/exercise-02/start-ps.sh
@@ -1,5 +1,5 @@
 bazel-bin/inception/imagenet_distributed_train \
 --job_name='ps' \
 --task_id=0 \
---ps_hosts='localhost:2222' \
---worker_hosts='localhost:2222'
+--ps_hosts='hpc0:2222' \
+--worker_hosts='hpc1:2222,hpc2:2222,hpc3:2222'
