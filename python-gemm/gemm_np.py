80b21b972157375c6ac5f8e0fbf9e29b375f654c
cyliustack <cyliustack@gmail.com>
2017-07-10 12:01:09 +0800

    autoformat python gemm

--- python-gemm/gemm_np.py
+++ python-gemm/gemm_np.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
-import sys, getopt
+import sys
+import getopt
 import numpy as np
+
+
 def main(argv):
     width = 0
     try:
-        opts, args = getopt.getopt(argv,"hw:",["width="])
+        opts, args = getopt.getopt(argv, "hw:", ["width="])
     except getopt.GetoptError:
         print 'gemm.py -w <width>'
         sys.exit(2)
@@ -14,17 +17,18 @@ def main(argv):
             sys.exit()
         elif opt in ("-w", "--width"):
             width = int(arg)
-    
+
     if width == 0:
         print 'gemm.py -w <width>'
         exit(0)
     else:
-        print('Width of square matrix is %d' % (width) )
-        a = np.random.rand(width,width)
-        b = np.random.rand(width,width)
+        print('Width of square matrix is %d' % (width))
+        a = np.random.rand(width, width)
+        b = np.random.rand(width, width)
         c = np.dot(a, b)
-        #print(c)
+        # print(c)
         print("Done.")
 
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
