5389c0794204032c76ed4f621b2d778f5866b1fc
cyliustack <cyliustack@gmail.com>
2017-07-15 16:56:16 +0800

    Update gemm

--- python-gemm/gemm.cpp
+++ python-gemm/gemm.cpp
@@ -1,12 +1,21 @@
-#include <iostream>
 #include <Eigen/Dense>
-using Eigen::MatrixXd;
-int main()
+#include <iostream>
+using namespace Eigen;
+using namespace std;
+int main(int argc, char* argv[])
 {
-  MatrixXd m(2,2);
-  m(0,0) = 3;
-  m(1,0) = 2.5;
-  m(0,1) = -1;
-  m(1,1) = m(1,0) + m(0,1);
-  std::cout << m << std::endl;
+
+    if (argc < 2) {
+        printf("Usage : ./matrix width_of_square_matrix\n");
+    }
+    uint32_t width = atoi(argv[1]);
+
+    MatrixXd m = MatrixXd::Random(width, width);
+    m = (m + MatrixXd::Constant(width, width, 1.2)) * 50;
+    //cout << "m =" << endl
+    //     << m << endl;
+    //cout << "m * m =" << endl
+    //     << m * m << endl;
+    m = m*m;
+    return 0;
 }
