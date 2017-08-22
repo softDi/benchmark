98f6330fbbf68837b2a10387dc4ff8070be6c312
cyliustack <cyliustack@gmail.com>
2017-08-23 00:48:52 +0800

    Update

--- father/tf_cnn/start-ps.sh
+++ father/tf_cnn/start-ps.sh
@@ -1,5 +1,11 @@
-python tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=2 \
+if [ "$1" == "" ] ; then 
+	echo "Usage: ./start-ps.sh NodeID"
+	exit -1
+else 
+	nid=$1
+	python tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=2 \
 --batch_size=64 --model=resnet50 --variable_update=distributed_replicated \
 --job_name=ps --ps_hosts=192.168.0.100:50000,192.168.0.101:50000 \
---worker_hosts=192.168.0.100:50001,192.168.0.101:50001 --task_index=0 \
+--worker_hosts=192.168.0.100:50001,192.168.0.101:50001 --task_index=${nid} \
 --data_dir=/mnt/dataset/imagenet
+fi 
---
+++ father/tf_cnn/stop-all.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ pssh -PH "192.168.0.100 192.168.0.101" pkill -x python
