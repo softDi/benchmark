c7a395305e721688996bbadfddb33c04e975e77c
cyliustack <cyliustack@gmail.com>
2017-08-23 00:08:52 +0800

    Add father/tf_cnn

--- father/exercise-02/start-workers.sh
+++
@@ -1,7 +0,0 @@
-bazel-bin/inception/imagenet_distributed_train \
---batch_size=128 \
---data_dir=/mnt/qnap/ImageNet_Dataset/imagenet-data \
---job_name='worker' \
---task_id=0 \
---ps_hosts='hpc0:2222' \
---worker_hosts='hpc1:2222,hpc2:2222,hpc3:2222'
---
+++ father/tf_cnn/start-workers.sh
@@ -0,0 +1,4 @@
+python tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=2 \
+--batch_size=64 --model=resnet50 --variable_update=distributed_replicated \
+--job_name=worker --ps_hosts=192.168.0.100:50000 \
+--worker_hosts=192.168.0.100:50001,192.168.0.101:50001 --task_index=0
