8a623d0d6a2191b6135f60e2f6ddc940211e9073
cyliustack <cyliustack@gmail.com>
2017-08-23 00:33:03 +0800

    Update ps worker and start-all

--- father/tf_cnn/start-workers.sh
+++ father/tf_cnn/start-workers.sh
@@ -1,4 +1,11 @@
-python tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=2 \
+#!/bin/bash
+if [ "$1" == "" ] ; then 
+	echo "Usage: ./start-worker.sh NodeID"
+	exit -1
+else 
+	nid=$1
+	python tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=2 \
 --batch_size=64 --model=resnet50 --variable_update=distributed_replicated \
 --job_name=worker --ps_hosts=192.168.0.100:50000 \
---worker_hosts=192.168.0.100:50001,192.168.0.101:50001 --task_index=0
+--worker_hosts=192.168.0.100:50001,192.168.0.101:50001 --task_index=${nid}
+fi
