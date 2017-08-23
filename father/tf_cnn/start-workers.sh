6d13e4c7aaae34e9e30de4791ae3a5e6518a8243
cyliustack <cyliustack@gmail.com>
2017-08-23 13:13:13 +0800

    Update default batch-szie

--- father/tf_cnn/start-workers.sh
+++ father/tf_cnn/start-workers.sh
@@ -5,7 +5,7 @@ if [ "$1" == "" ] ; then
 else 
 	nid=$1
 	python tf_cnn_benchmarks.py --local_parameter_device=gpu --num_gpus=2 \
---batch_size=64 --model=resnet50 --variable_update=distributed_replicated \
+--batch_size=32 --model=resnet50 --variable_update=distributed_replicated \
 --job_name=worker --ps_hosts=192.168.0.100:50000,192.168.0.101:50000 \
 --worker_hosts=192.168.0.100:50001,192.168.0.101:50001 --task_index=${nid}
 fi
