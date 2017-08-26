2d519144b8a2802e5da8ade724a56bd17f594376
cyliustack <cyliustack@gmail.com>
2017-08-26 16:22:23 +0800

    Change defautl variable management

--- father/tf_cnn/start-workers.sh
+++ father/tf_cnn/start-workers.sh
@@ -8,7 +8,7 @@ else
 	batch_size=$3
 	num_gpus=$4
 	python tf_cnn_benchmarks.py --local_parameter_device=cpu --num_gpus=${num_gpus} \
---batch_size=${batch_size} --model=${model} --variable_update=distributed_replicated \
+--batch_size=${batch_size} --model=${model} --variable_update=parameter_server \
 --job_name=worker --ps_hosts=192.168.0.100:50000,192.168.0.101:50000 \
 --worker_hosts=192.168.0.100:50001,192.168.0.101:50001 --task_index=${nid} 
 fi
