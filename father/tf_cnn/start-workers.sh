d5fee08c95355f3714f4a13ee0d513f1e6e432b7
cyliustack <cyliustack@gmail.com>
2017-08-26 15:30:07 +0800

    Bug fixed

--- father/tf_cnn/start-workers.sh
+++ father/tf_cnn/start-workers.sh
@@ -9,6 +9,6 @@ else
 	num_gpus=$4
 	python tf_cnn_benchmarks.py --local_parameter_device=cpu --num_gpus=${num_gpus} \
 --batch_size=${batch_size} --model=${model} --variable_update=distributed_replicated \
---job_name=worker --ps_hosts=192.168.0.100:50000,192.168.0.101:50000, \
+--job_name=worker --ps_hosts=192.168.0.100:50000,192.168.0.101:50000 \
 --worker_hosts=192.168.0.100:50001,192.168.0.101:50001 --task_index=${nid} 
 fi
