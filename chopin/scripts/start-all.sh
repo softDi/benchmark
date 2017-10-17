ba8da70e29f092c5574d6b891a5837f4543d9910
Your Name <you@example.com>
2017-10-17 22:01:28 +0800

    Rename father to chopin

---
+++ chopin/scripts/start-all.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ "$(hostname)" == "hpc0" ] ; then 
+ echo "This is Node-$( hostname | sed 's/hpc//g') "
+ echo "Act as a parameter server." && exit 0
+python trainer.py \
+    --ps_hosts=hpc0:2222 \
+    --worker_hosts=hpc1:2222,hpc2:2222,hpc3:2222 \
+    --job_name=ps --task_index=0
+else 
+    echo "This is Node-$(hostname | sed 's/hpc//g') "
+    echo "Act as a worker." && exit 0
+    python trainer.py \
+    --ps_hosts=hpc0:2222 \
+    --worker_hosts=hpc1:2222,hpc2:2222,hpc3:2222 \
+    --job_name=worker --task_index=$( hostname | sed 's/hpc//g')
+fi 
+
