a614a670ea1e61ad68217e48f4ba6a99ba5194f5
Your Name <you@example.com>
2017-10-20 12:49:26 +0000

    update demo-npb to support more npb programs

--- mpi/tools/demo-npb.sh
+++ mpi/tools/demo-npb.sh
@@ -1,4 +1,51 @@
 #!/bin/bash 
-~/benchmark/tools/ssh_copy_id_to_all.sh 4
-~/benchmark/tools/genhosts.sh 4
-mpirun -n 4 -f hosts.txt benchmark/mpi/NPB3.3-MPI/bin/bt.A.4
+C_NONE="\033[0m"
+C_CYAN="\033[36m"
+C_RED="\033[31m"
+C_GREEN="\033[32m"
+C_ORANGE="\033[33m"
+C_BLUE="\033[34m"
+C_PURPLE="\033[35m"
+C_CYAN="\033[36m"
+C_LIGHT_GRAY="\033[37m"
+
+print_misc() {
+    echo -e "${C_PURPLE} $1 ${C_NONE}"
+}
+
+print_info() {
+    echo -e "${C_BLUE} $1 ${C_NONE}"
+}
+
+print_error() {
+    echo -e "${C_RED} $1 ${C_NONE}"
+}
+
+print_warning() {
+    echo -e "${C_ORANGE} $1 ${C_NONE}"
+}
+
+print_misc "Bash version ${BASH_VERSION}."
+
+if [[ "$1" == "" ]] || [[ "$2" == "" ]] || [[ "$3" == "" ]]
+then
+	print_error "Usage: ./demo-npb.sh name class num_processes"
+	exit -1
+fi
+
+NAME=$1
+CLASS=$2
+NUM_PROCESSES=$3  
+~/benchmark/tools/ssh_copy_id_to_all.sh ${NUM_PROCESSES}
+~/benchmark/tools/genhosts.sh ${NUM_PROCESSES}
+pssh -h hosts.txt "make -C ~/benchmark/mpi/NPB3.3-MPI ${NAME} ${CLASS} ${NUM_PROCESSES}"
+if [[ ! -f benchmark/mpi/NPB3.3-MPI/bin/${NAME}.${CLASS}.${NUM_PROCESSES} ]]
+then
+	print_error "Cannot find ${NAME}.${CLASS}.${NUM_PROCESSES}"
+	exit -1
+fi
+mpirun -n ${NUM_PROCESSES} -f hosts.txt benchmark/mpi/NPB3.3-MPI/bin/${NAME}.${CLASS}.${NUM_PROCESSES}
+
+
+
+
